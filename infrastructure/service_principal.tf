54735b34d4098631bcbd764e5bba8a1b11fa7cd8
Akinlolu Akindele <AAkindele@users.noreply.github.com>
2020-07-29 10:33:34 -0500

    issue #34 add service principal password for cert-manager to terraform (#46)

--- infrastructure/service_principal.tf
+++ infrastructure/service_principal.tf
@@ -11,3 +11,14 @@ resource "azurerm_role_assignment" "cert-manager-dns-zone" {
   role_definition_name = "DNS Zone Contributor"
   principal_id         = azuread_service_principal.cert-manager.object_id
 }
+
+resource "random_password" "password" {
+  length  = 32
+  special = true
+}
+
+resource "azuread_service_principal_password" "cert-manager-password" {
+  service_principal_id = azuread_service_principal.cert-manager.id
+  value                = random_password.password.result
+  end_date_relative    = "17520h" #expire in 2 years
+}
